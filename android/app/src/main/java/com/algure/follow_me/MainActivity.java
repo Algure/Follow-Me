b114f0d407b4ce8a9ae024ac623cb817dbefe992
Algure <ajirie98@gmail.com>
2021-05-21 04:56:02 +0100

    built web release

---
+++ android/app/src/main/java/com/algure/follow_me/MainActivity.java
@@ -0,0 +1,6 @@
+package com.algure.follow_me;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ android/app/src/main/kotlin/com/algure/follow_me/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.algure.follow_me
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
